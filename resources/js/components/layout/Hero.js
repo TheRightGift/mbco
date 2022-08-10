50eafd485a8176ed61b28c0eef56dfb73baa2f08
Chigozie Chukwu <goziechukwu@gmail.com>
2022-08-10 09:36:02 +0100

    Improve theme and fixed bug with modal

--- resources/js/components/layout/Hero.js
+++ resources/js/components/layout/Hero.js
@@ -1,28 +1,28 @@
 import React from 'react';
-
+import Register from '../pages/Register';
 
 import imagehero from '../layout/heroimage.png'
 const Hero = () => {
   return (
-    <section className='hero'>
-  
-      <div className=" container hero-grid">
-        <div className="hero-content">
-       
-        <h1>We are <span className='primary-color'>MBCON</span></h1>
+    <>
+      <section className='hero'>  
+        <div className=" container hero-grid">
+          <div className="hero-content">
+            <h1>We are <span className='primary-color'>MBCON</span></h1>
 
 
-        <p className='lead'>The Mercedes-Benz Club of Naija (MBCON) is an automative club made up of enthusiasts brought together by the love for Mercedes-Benz, one of the leading motoring companies in the world.</p>
-          <div className='hero-button'>
-          <a href="#register" className="primary-bg btn modal-trigger register">Become a member</a>
+            <p className='lead'>The Mercedes-Benz Club of Naija (MBCON) is an automative club made up of enthusiasts brought together by the love for Mercedes-Benz, one of the leading motoring companies in the world.</p>
+            <div className='hero-button'>
+              <a href="#register" className="primary-bg btn modal-trigger register">Become a member</a>
+            </div>
           </div>
+          <div className="hero-img">
+            <img src={imagehero} alt="" className='image-hero' />
+          </div>     
         </div>
-        <div className="hero-img">
-          <img src={imagehero} alt="" className='image-hero' />
-          </div>
-     
-    </div>
-    </section>
+      </section>
+      <Register />
+    </>
   )
 }
 
--- resources/js/components/layout/membershipagelayout/Benefitjoin.js
+++ resources/js/components/layout/membershipagelayout/Benefitjoin.js
@@ -2,29 +2,29 @@ import React from 'react'
 import logo from "../membershipagelayout/logo.png"
 import "animate.css/animate.min.css";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
+import Register from '../../pages/Register';
 
 const Benefitjoin = () => {
   return (
     <section className='aboutprofile'>
     <div className="container">
-    <AnimationOnScroll animateIn="animate__zoomIn">
-      <h2 className='secondary-heading'>
-      Benefits of Joining MBCON
-      </h2>
-    <ul className="benefit-list">
-      <li><span className='benefit-item'> <img src={logo} alt="" className="logo" /></span> <span>  We are committed to offering members, family, friends, guests and other interested parties opportunities in social establishments to participate in events with Mercedes-Benz and other branded vehicles deemed appropriate at local and international levels  </span>
-     </li>
-      <li><span className='benefit-item'> <img src={logo} alt="" className="logo"/></span><span>  Serve the interests of members through official means that are obtainable in-country and give back to the society. </span>
-    </li>
-      <li><span className='benefit-item'> <img src={logo} alt="" className="logo" /></span> <span>   We have provided a safe haven with an umbrella for fellow enthusiasts who love motorsports as well as own and love the brand Mercedes Brand.</span>
-     </li>
-      <li><span className='benefit-item'> <img src={logo} alt="" className="logo" /></span><span>Above all, we are open for business, we are willing to work with other organisations to promote the “car culture” and build a thriving automobile community in Nigeria and Africa. </span>
-      </li>
-    </ul>
-<div className='hero-button'>
-<a href="#register" className="secondary-bg btn modal-trigger register">Become a member</a>
-        </div></ AnimationOnScroll >
-
+      <AnimationOnScroll animateIn="animate__zoomIn">
+        <h2 className='secondary-heading'>Benefits of Joining MBCON</h2>
+        <ul className="benefit-list">
+          <li><span className='benefit-item'> <img src={logo} alt="" className="logo" /></span> <span>  We are committed to offering members, family, friends, guests and other interested parties opportunities in social establishments to participate in events with Mercedes-Benz and other branded vehicles deemed appropriate at local and international levels  </span>
+          </li>
+          <li><span className='benefit-item'> <img src={logo} alt="" className="logo"/></span><span>  Serve the interests of members through official means that are obtainable in-country and give back to the society. </span>
+          </li>
+          <li><span className='benefit-item'> <img src={logo} alt="" className="logo" /></span> <span>   We have provided a safe haven with an umbrella for fellow enthusiasts who love motorsports as well as own and love the brand Mercedes Brand.</span>
+          </li>
+          <li><span className='benefit-item'> <img src={logo} alt="" className="logo" /></span><span>Above all, we are open for business, we are willing to work with other organisations to promote the “car culture” and build a thriving automobile community in Nigeria and Africa. </span>
+          </li>
+        </ul>
+        <div className='hero-button'>
+          <a href="#register" className="secondary-bg btn modal-trigger register">Become a member</a>
+        </div>
+      </AnimationOnScroll >
+      <Register/>
     </div>
   </section>
   )
